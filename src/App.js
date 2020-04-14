633c82990891c03d65a16b14affb96828b2da903
Sajan Shrestha <sajan.stha1437@gmail.com>
2020-04-15 00:50:18 +0900

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Cards, Chart, Country } from './components';
+import styles from './App.module.css';
+import { fetchData } from './api';
+import coronaImage from'./images/corona.png';
+
+class App extends React.Component{
+
+    /* In order to display the data from ComponentDidMount to Cards We need to use state*/
+
+    state = {
+        data:{}, // Initially this data is empty until it is populated by the request inside ComponentDidMount
+        country:'',
+    }
+
+    async componentDidMount(){
+        const fetchedData = await fetchData();
+
+        this.setState({data:fetchedData});
+        
+    }
+
+    handleCountryChange = async (country)=>{
+
+         //fetch data
+        const fetchedData = await fetchData(country);
+
+        //set the state
+        this.setState({ data: fetchedData, country: country});
+    }
+
+    render(){
+
+        const { data , country} = this.state;
+        return(
+
+            <div className={styles.container}>
+                
+                <img src={coronaImage} className={styles.image} alt="COVID-19"/>
+                               
+                <Cards className={styles.cards} data = {data}/>
+                <Country handleCountryChange={this.handleCountryChange}/>
+                <Chart data = {data} country = {country}/>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Country.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import { NativeSelect, FormControl } from '@material-ui/core'; 
+
+import styles from './css/Country.module.css';
+import { fetchCountries } from '../api';
+
+
+const Country = ({handleCountryChange}) => {
+
+    const [fetchedCountries, setFetchedCountries] =useState([]);
+    useEffect(()=>{
+        const fetchAPI = async()=>{
+            setFetchedCountries(await fetchCountries());
+
+        }
+        fetchAPI();
+    },[setFetchedCountries]);
+
+    return (
+        <FormControl className={styles.formControl}>
+            <NativeSelect defaultValue="" onChange={(e)=>handleCountryChange(e.target.value)}>
+                
+                <option value="">Worldwide</option>
+                {fetchedCountries.map((country, i)=><option key={i} value={country}>{country}</option>)}
+            </NativeSelect>
+        </FormControl>
+    ); 
+}
+
+export default Country;
